a2494da5aeffb799c000221c38952cf69bd16d4b
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-03-27 17:19:55 +0000

    Initial buggy version

---
+++ lib/Task/Bot/Training.pm
@@ -0,0 +1,43 @@
+package Task::Bot::Training;
+
+use 5.010;
+
+use Bot::Training;
+use Bot::Training::MegaHAL;
+use Bot::Training::StarCraft;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Task::Bot::Training - Install all the L<Bot::Training> training files
+
+=head1 DESCRIPTION
+
+This module installs all the currently known L<Bot::Training>
+modules. Currently these are:
+
+=over
+
+=item * L<Bot::Training>
+
+=item * L<Bot::Training::MegaHAL>
+
+=item * L<Bot::Training::StarCraft>
+
+=cut
+
+=head1 AUTHORS
+
+E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason <avar@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason <avar@cpan.org>
+
+This program is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
